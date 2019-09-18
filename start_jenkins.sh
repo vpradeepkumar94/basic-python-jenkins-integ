d2ca87d162723b88ad11de2eac35b18ee81b37e9
Pradeep Kumar V <vpradeepkumar94@gmail.com>
2019-09-18 09:21:51 +0530

    changes

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,1 +1,1 @@
-docker run -p8080:8080 -p:5000:5000 -v ${PWD}/jenkins_volume:/var/jenkins_home f32b4bb22e4d
+docker run -p8080:8080 -p:5000:5000 -v ${PWD}/jenkins_volume:/var/jenkins_home -v ${PWD}/project:/var/jenkins_home/project f32b4bb22e4d
